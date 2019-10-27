58712d01ecb3cbd3ddced625b199f32aa3ddf2a0
dzwebcreations <dzwebcreations@gmail.com>
2019-10-27 14:05:30 -0400

    Initial Commit

---
+++ lib/models/client.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class Client {
+  final String name;
+  final String descript;
+
+  Client(
+    {
+     @required this.name,
+     @required this.descript,
+      }
+    );
+}
