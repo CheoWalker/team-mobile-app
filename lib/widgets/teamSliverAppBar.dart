817c4484107c4d95b5b1a4c90015d7c7b5c14b1c
dzwebcreations <dzwebcreations@gmail.com>
2019-10-27 19:56:39 -0400

    Move the SliverAppBar to its own file

---
+++ lib/widgets/teamSliverAppBar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class TeamSliverAppBar extends SliverPersistentHeaderDelegate {
+  final double expandedHeight;
+
+  TeamSliverAppBar({@required this.expandedHeight});
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Stack(
+      fit: StackFit.expand,
+      overflow: Overflow.visible,
+      children: [
+        Image.network(
+          "https://media.graytvinc.com/images/810*455/packers+logo3.jpg",
+          fit: BoxFit.cover,
+        ),
+        Center(
+          child: Opacity(
+            opacity: shrinkOffset / expandedHeight,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Color(0xFF203731),
+              child: Center(
+                child: Text(
+                  "MySliverAppBar",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 23,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  double get maxExtent => expandedHeight;
+
+  @override
+  double get minExtent => kToolbarHeight;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+}
