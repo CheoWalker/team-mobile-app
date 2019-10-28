6a9ee65260412a29971232a5b479b7872c8a33c8
dzwebcreations <dzwebcreations@gmail.com>
2019-10-27 22:06:09 -0400

    Added sample DataTable

--- lib/pages/standingpage.dart
+++ lib/pages/standingpage.dart
@@ -11,25 +11,39 @@ class _StandingPageState extends State<StandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: CustomScrollView(
-        slivers: [
-          SliverPersistentHeader(
-            delegate: TeamSliverAppBar(
-              expandedHeight: 150,
-              pageTitle: 'Team Standing',
-            ),
-            pinned: true,
-          ),
-          SliverToBoxAdapter(
-            child: SingleChildScrollView(
-              child: DataTable(
-                columns: [],
-                rows: [],
+        child: CustomScrollView(
+          slivers: [
+            SliverPersistentHeader(
+              delegate: TeamSliverAppBar(
+                expandedHeight: 150,
+                pageTitle: 'Team Standing',
               ),
+              pinned: true,
             ),
-          )
-        ],
-      )),
+            SliverToBoxAdapter(
+              child: SingleChildScrollView(
+                child: DataTable(
+                  columnSpacing: 30,
+                  columns: [
+                    DataColumn(label: Text('Team')),
+                    DataColumn(label: Text('Wins')),
+                    DataColumn(label: Text('Loses')),
+                    DataColumn(label: Text('Ties')),
+                  ],
+                  rows: [
+                    DataRow(cells: [
+                      DataCell(Text('Green Bay')),
+                      DataCell(Text('7')),
+                      DataCell(Text('1')),
+                      DataCell(Text('0')),
+                    ])
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
